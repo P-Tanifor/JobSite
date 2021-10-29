afc5b0acd3a6e81fe96f7486a661705fa86b933e
Paulinus Tanifor <ptanifor@gmail.com>
2021-10-29 04:32:45 -0700

    rm secret key

--- JobSite/settings.py
+++ JobSite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l=k1scku0krc_xz=dw2t%l^36=hsfp%el_1&=^3zgmswp2-wyg'
+#
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
